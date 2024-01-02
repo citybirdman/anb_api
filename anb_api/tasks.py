72fd050d0e89e6b1598c4eb7bc34bb6fe482bffe
Ahmed Zaytoon <citybirdman@gmail.com>
2024-01-02 15:31:38 +0000

    update

--- anb_api/tasks.py
+++ anb_api/tasks.py
@@ -3,12 +3,12 @@ import requests
 @frappe.whitelist()
 def get_balance():
     settings = frappe.get_doc("ANB Settings")
-    settings.start_connection()
+    response = settings.start_connection()
     headers = {
-        "Authorization": f"Bearer {settings['access_token']}",
+        "Authorization": f"Bearer {response['access_token']}",
         "Accept": "application/json"
     }
     account_number = "0108095164500016"
     from_date = "2023-12-25"
     auth = requests.get(f"https://test-api.anb.com.sa/v2/report/account/statement?accountNumber={account_number}&offset=Offset&type=JSON&fromDate={from_date}", headers=headers)
-    return auth.text
+    return auth
