238471198efa2dcb784ff566a1125fd5ffe5e574
Ahmed Zaytoon <citybirdman@gmail.com>
2024-01-03 14:18:15 +0000

    test

--- anb_api/tasks.py
+++ anb_api/tasks.py
@@ -2,12 +2,7 @@ import frappe
 import json
 import requests
 from frappe.utils import add_days, today
-def get_account_statment(account_number, settings, try_again=True):
-    response = json.loads(settings.start_connection().text)
-    headers = {
-        "Authorization": f"Bearer {response['access_token']}",
-        "Accept": "application/json"
-    }
+def get_account_statment(account_number, settings, try_again=True, headers={}):
     from_date = add_days(today(), -7) 
     response = requests.get(f"https://test-api.anb.com.sa/v2/report/account/statement?accountNumber={account_number}&offset=Offset&type=JSON&fromDate={from_date}", headers=headers)
     if response.status_code != 200:
@@ -20,8 +15,13 @@ def get_account_statment(account_number, settings, try_again=True):
 
 @frappe.whitelist()
 def get_statments():
+    response = json.loads(settings.start_connection().text)
+    headers = {
+        "Authorization": f"Bearer {response['access_token']}",
+        "Accept": "application/json"
+    }
     settings = frappe.get_doc("ANB Settings")
     results = []
     for account in settings.accounts:
-        results.append(get_account_statment(account.account_number, settings, True))
+        results.append(get_account_statment(account.account_number, settings, True, headers))
     return results
