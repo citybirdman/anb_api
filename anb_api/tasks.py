59af291685c2838c4d1acae1868bfadc369dd9ae
Ahmed Zaytoon <citybirdman@gmail.com>
2024-01-03 09:53:14 +0000

    test

--- anb_api/tasks.py
+++ anb_api/tasks.py
@@ -1,7 +1,8 @@
 import frappe
 import json
 import requests
-@frappe.whitelist()
+from frappe.utils import add_days, today
+@frappe.whitelist(try_again=True)
 def get_balance():
     settings = frappe.get_doc("ANB Settings")
     response = json.loads(settings.start_connection().text)
@@ -10,6 +11,11 @@ def get_balance():
         "Accept": "application/json"
     }
     account_number = "0108095164500016"
-    from_date = "2023-12-25"
-    auth = requests.get(f"https://test-api.anb.com.sa/v2/report/account/statement?accountNumber={account_number}&offset=Offset&type=JSON&fromDate={from_date}", headers=headers)
-    return auth
+    from_date = add_days(today(), -7) 
+    response = requests.get(f"https://test-api.anb.com.sa/v2/report/account/statement?accountNumber={account_number}&offset=Offset&type=JSON&fromDate={from_date}", headers=headers)
+    if response.status_code != 200:
+        if try_again:
+            try_again = False
+            get_balance()
+    else:
+        return response.text
