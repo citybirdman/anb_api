d377eaf42dcadf1a4d1e023c0c11e12a56b598bd
Ahmed Zaytoon <citybirdman@gmail.com>
2024-01-08 07:34:35 +0000

    add custom field

---
+++ anb_api/install.py
@@ -0,0 +1,22 @@
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+
+def after_install():
+	create_parking_account_field()
+
+def create_parking_account_field():
+	print("In the Patch")
+	custom_field = {
+		"Customer": [
+			dict(
+				fieldname="anb_bank_account",
+				label="ANB Bank Account",
+				fieldtype="Data",
+				insert_after="stock_received_but_not_billed",
+				ignore_user_permissions=1,
+				unique=1,
+				no_copy=1,
+			)
+		]
+	}
+	create_custom_fields(custom_field, ignore_validate=frappe.flags.in_patch, update=True)
